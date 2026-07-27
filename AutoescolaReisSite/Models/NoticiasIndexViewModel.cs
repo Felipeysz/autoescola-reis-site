65137205553cb545a8777acb51002f6c39f46bae
Felipeysz <Felipesantana87146520@gmail.com>
2026-07-27 05:02:14 -0300

    Paginação real na lista de Notícias (6 por página, com Skip/Take sobre NoticiasData)

---
+++ AutoescolaReisSite/Models/NoticiasIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿// Models/NoticiasIndexViewModel.cs
+namespace AutoescolaReisSite.Models
+{
+    public class NoticiasIndexViewModel
+    {
+        public List<NoticiaPost> Posts { get; set; } = new();
+        public int PaginaAtual { get; set; } = 1;
+        public int TotalPaginas { get; set; } = 1;
+
+        public bool TemPaginaAnterior => PaginaAtual > 1;
+        public bool TemProximaPagina => PaginaAtual < TotalPaginas;
+    }
+}

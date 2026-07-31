e8f3ec67a28f08b00790f4c541ed95bd423d0150
Felipeysz <Felipesantana87146520@gmail.com>
2026-07-31 06:08:05 -0300

    Setup do projeto AutoescolaReisSite.Crm (.NET 10, Controllers) + Docker Compose local com Postgres, health-check funcionando via container

---
+++ AutoescolaReisSite.Crm/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿// Controllers/HealthController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoescolaReisSite.Crm.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "ok", servico = "AutoescolaReisSite.Crm" });
+        }
+    }
+}
